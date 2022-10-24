8694fc6a5a1c91e2438e5f0f02051ab32a6d2445
Jwanani <ajh8505@gmail.com>
2022-10-24 11:06:23 +0900

    컨트랙트 마이그레이션

---
+++ contract/contracts/KnockSeaNFT.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract KnockSeaNFT is ERC721URIStorage, Ownable, ERC721Enumerable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    address marketContractAddress;
+
+    constructor(address contractAddress) ERC721("KnockSeaNFT", "NFT"){
+        marketContractAddress = contractAddress;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal
+      override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal
+      override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view
+      override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(_tokenId);
+    }
+    
+    function mintNFT(address _recipient, string memory _tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(_recipient, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        setApprovalForAll(marketContractAddress, true); //grant transaction permission to market
+
+        return newItemId;
+    }
+}
